062b4375ed9cde29961359b19fe7408f774fca2b
John Hooley <webfonts@bluebridgedev.com>
2011-10-19 07:49:33 -0700

    Made the install.sql file less zealous in dropping tables for smoother upgrades

--- com_webfonts/admin/install.sql
+++ com_webfonts/admin/install.sql
@@ -1,5 +1,3 @@
-DROP TABLE IF EXISTS `#__webfonts`;
-
 CREATE TABLE IF NOT EXISTS `#__webfonts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `selector` varchar(300) NOT NULL COMMENT 'css selector',
@@ -18,9 +16,9 @@ CREATE TABLE IF NOT EXISTS `#__webfonts_vendor` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0;
 
-INSERT INTO `#__webfonts_vendor` (`id`, `name`, `properties`) VALUES (1, 'Fonts.com', '{"account":{"email":"","firstName":"","lastName":""},"key":"","designers":{},"foundries":{},"classifications":{},"languages":{},"wfspid":""}');
-
-DROP TABLE IF EXISTS `#__webfonts_webfonts`;
+INSERT INTO `#__webfonts_vendor` (`id`, `name`, `properties`) VALUES 
+(1, 'Fonts.com', '{"account":{"email":"","firstName":"","lastName":""},"key":"","designers":{},"foundries":{},"classifications":{},"languages":{},"wfspid":""}'),
+(2, 'Google Web Fonts', '{"hash":""}');
 
 CREATE TABLE IF NOT EXISTS `#__webfonts_fontscom` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -29,3 +27,12 @@ CREATE TABLE IF NOT EXISTS `#__webfonts_fontscom` (
   `font` text CHARACTER SET utf8 NOT NULL COMMENT 'json encoded font info',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=0 ;
+
+CREATE TABLE `#__webfonts_google` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`kind` VARCHAR( 100 ) NOT NULL ,
+`name` VARCHAR( 150 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+`hash` VARCHAR( 32 ) NOT NULL ,
+`variant` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL ,
+`subset` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL 
+) ENGINE = MYISAM ;
